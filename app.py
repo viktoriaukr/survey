4906ce17f9f080db3b8f562a9dcd4ee447caadf7
viktoriaukr <116932692+viktoriaukr@users.noreply.github.com>
2023-06-10 01:47:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
+
+
 RESPONSES = 'responses'
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
